af25fa1bdd76e55fab8fdb92c73e993df97beff7
Theophilus Awoyele <theophilusawoyele@gmail.com>
2022-11-25 11:41:24 +0000

    Setting up the homepage with dummy data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {Container} from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
-     <h1>Welcome to Olatunde's shop</h1> 
+      <Header />
+      <main className='py-4'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main> 
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/products.js
@@ -0,0 +1,53 @@
+
+const products = [
+{
+    _id: '1',
+    name: 'Airpods Pro',
+    image: '/images/airpods.jpeg',
+    description: 'Earphones for music',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 250,
+    countInStock: 9,
+    rating: 4.6,
+    numReviews: 3,
+},
+{
+    _id: '2',
+    name: 'Apple Airtag',
+    image: '/images/airtag.jpeg',
+    description: 'To track',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 100,
+    countInStock: 8,
+    rating: 4.9,
+    numReviews: 4,
+},
+{
+    _id: '3',
+    name: 'Macbook Pro',
+    image: '/images/macbook.jpeg',
+    description: 'Personal computer',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 1250.45,
+    countInStock: 7,
+    rating: 4.2,
+    numReviews: 2,
+},
+{
+    _id: '4',
+    name: 'iPhone 14 Pro Max',
+    image: '/images/iphone14.jpeg',
+    description: 'Best mobile phone',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 980,
+    countInStock: 6,
+    rating: 3.8,
+    numReviews: 7,
+},
+]
+
+export default products
